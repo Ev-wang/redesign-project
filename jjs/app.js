287986362c440f1251c622bb0309195487be5ef9
Calberts4 <42820351+Calberts4@users.noreply.github.com>
2018-10-06 13:48:34 -0700

    updated the tab list and the ccss and jjs is my own custom css page

---
+++ jjs/app.js
@@ -0,0 +1,26 @@
+$(document).foundation()
+
+
+
+
+$(document).mouseup(function (e) {
+  var container = $('#contact-panel');
+  if (!container.is(e.target) // if the target of the click isn't the container...
+  && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+      container.removeClass('is-active');
+    }
+});
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
